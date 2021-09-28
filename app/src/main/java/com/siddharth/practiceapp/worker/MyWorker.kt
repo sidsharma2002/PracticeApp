03b5d17cb7ada7ca581fdcc78193c6e8b2661fd9
Pranav <pranavtiwari12j@gmail.com>
2021-09-28 15:48:00 +0530

    Added comments in MyWorker.kt file

--- app/src/main/java/com/siddharth/practiceapp/worker/MyWorker.kt
+++ app/src/main/java/com/siddharth/practiceapp/worker/MyWorker.kt
@@ -2,24 +2,27 @@ package com.siddharth.practiceapp.worker
 
 
 import android.app.NotificationManager
-import  com.siddharth.practiceapp.util.sendNotification
 import android.content.Context
 import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.siddharth.practiceapp.api.RetrofitInstance.Companion.api
+import com.siddharth.practiceapp.util.sendNotification
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
+// Setting up Coroutine Worker for API Call
+
 class MyWorker(appContext: Context, workerParams: WorkerParameters) :
-    CoroutineWorker(appContext,workerParams) {
+    CoroutineWorker(appContext, workerParams) {
 
     private val TAG = "MyWorker : "
 
     override suspend fun doWork(): Result {
         return try {
-            withContext(Dispatchers.IO){
+            // Coroutine execution to I/O Dispatcher
+            withContext(Dispatchers.IO) {
                 fetchData()
             }
             Result.success()
@@ -29,17 +32,30 @@ class MyWorker(appContext: Context, workerParams: WorkerParameters) :
         }
     }
 
+    /**
+     * This function fetches the top news from the api and
+     * Shows the fetched news as Notification using showNotification().
+     */
+
     private suspend fun fetchData() {
-            val rawData = api.getTopNews()
-            Log.d(TAG,rawData.isSuccessful.toString())
-            val results = rawData.body()
-            val news = results?.articles?.get(0)?.title
-            news?.let{
-                showNotification(news)
-            }
+        val rawData = api.getTopNews()
+        Log.d(TAG, rawData.isSuccessful.toString())
+        val results = rawData.body()
+
+        // fetching the title of first article from results.
+
+        val news = results?.articles?.get(0)?.title
+        news?.let {
+            showNotification(news)
+        }
     }
 
-    private fun showNotification(message : String) {
+    /**
+     * This function makes use of Notification manager
+     * To show the news fetched from the api as notification to the user.
+     */
+
+    private fun showNotification(message: String) {
         val notificationManager = ContextCompat.getSystemService(
             applicationContext,
             NotificationManager::class.java
