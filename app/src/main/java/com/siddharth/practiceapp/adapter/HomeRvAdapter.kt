5cf7feb56b6addc752aef0d2bd73b41bea4e3675
Siddharth sharma <siddharthsharma24.7.360@gmail.com>
2021-11-08 18:54:37 +0530

    cleanUp + new module

--- app/src/main/java/com/siddharth/practiceapp/adapter/HomeRvAdapter.kt
+++ app/src/main/java/com/siddharth/practiceapp/adapter/HomeRvAdapter.kt
@@ -5,9 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.siddharth.practiceapp.R
 import com.siddharth.practiceapp.data.entities.HomeData
 import com.siddharth.practiceapp.util.slideUp
@@ -30,15 +33,16 @@ class HomeRvAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         init {
             Log.d("NewsHolder", "created")
         }
-       private val newsHeadline: TextView = itemView.findViewById(R.id.tv_newsHeadline)
-       private val newsDescription: TextView = itemView.findViewById(R.id.tv_newsDesc)
-       // private val newsView = itemView as NewsItemView
+
+        private val newsHeadline: TextView = itemView.findViewById(R.id.tv_newsHeadline)
+        private val newsDescription: TextView = itemView.findViewById(R.id.tv_newsDesc)
+        // private val newsView = itemView as NewsItemView
 
         @RequiresApi(Build.VERSION_CODES.O)
         fun setData(data: HomeData, holder: NewsHolder, position: Int) {
-         holder.newsHeadline.text = data.title
-         holder.newsDescription.text = data.description
-         // newsView.populate(data)
+            holder.newsHeadline.text = data.title
+            holder.newsDescription.text = data.description
+            // newsView.populate(data)
         }
     }
 
@@ -46,10 +50,14 @@ class HomeRvAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         init {
             Log.d("ReminderHolder", "created")
         }
-        private val headline : TextView = itemView.findViewById(R.id.tv_headline)
+
+        private val headline: ImageView = itemView.findViewById(R.id.iv_image)
         fun setData(data: HomeData, holder: ReminderHolder) {
-            // holder.reminderText.text = textToSet
-            headline.slideUp(headline.context,800,250)
+            Glide.with(holder.headline.context)
+                .load("https://firebasestorage.googleapis.com/v0/b/firechat-5a222.appspot.com/o/Artboard%201.jpg?alt=media&token=45796976-3aff-4678-92b5-59592fad499f")
+                .into(holder.headline)
+            holder.itemView.isVisible = false
+            holder.itemView.slideUp(headline.context, 500, 250)
         }
     }
 
@@ -63,7 +71,7 @@ class HomeRvAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         }
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.item_news_home, parent, false)
-            // NewsItemView(parent.context)
+        // NewsItemView(parent.context)
         return NewsHolder(view)
     }
 
@@ -71,7 +79,7 @@ class HomeRvAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         Log.d("OnBindViewHolder", "fired")
         if (holder is NewsHolder) {
-            holder.setData(dataList[position],holder,position)
+            holder.setData(dataList[position], holder, position)
         } else if (holder is ReminderHolder) {
             holder.setData(dataList[position], holder)
         }
---
+++ configDep/src/main/java/Versions.kt
@@ -0,0 +1,70 @@
+object Versions {
+
+    const val gradleVersion = "7.0.3"
+    const val gradleNavigationArgVersion = "2.3.5"
+    const val kotlinVersion = "1.4.32"
+
+    // Core dependencies
+    const val javaxInjectVersion = "1"
+    const val coreKtxVersion = "1.3.2"
+    const val appCompatVersion = "1.2.0"
+    const val materialVersion = "1.3.0"
+    const val constraintLayoutVersion = "2.0.4"
+
+    // Activity KTX for viewModels() dependencies
+    const val activityKtxVersion = "1.1.0"
+
+    // Navigation component dependencies
+    const val navFragmentKtxVersion = "2.3.0"
+    const val navUiKtxVersion = "2.3.0"
+
+    // LifeCycle dependencies
+    const val viewmodelKtxVersion = "2.2.0"
+    const val lifeCycleExtVersion = "2.2.0"
+    const val liveDataKtxVersion = "2.2.0"
+    const val lifeCycleRuntimeVersion = "2.2.0"
+    const val lifeCycleRuntimeKtxVersion = "2.2.0"
+
+    // Coroutines dependencies
+    const val coroutineCoreVersion = "1.3.9"
+    const val coroutineAndroidVersion = "1.3.9"
+
+    //Glide dependencies
+    const val glideVersion = "4.11.0"
+
+    // Dagger - Hilt dependencies
+    const val hiltAndroidVersion = "2.35"
+    const val hiltVersion = "1.0.0-alpha02"
+
+    // Timber logging dependencies
+    const val timberVersion = "4.7.1"
+
+    // Network dependencies
+    const val retrofitVersion = "2.7.0"
+    const val moshiConverterVersion = "2.7.0"
+    const val loggingInterceptorVersion = "4.2.1"
+
+    // Room
+    const val roomVersion = "2.2.5"
+
+    // Lottie animation
+    const val lottieVersion = "3.7.0"
+
+    // Testing
+    const val junitVersion = "4.13"
+    const val androidxArchCoreVersion = "2.1.0"
+    const val coroutinesTestVersion = "1.4.3"
+    const val mockitoCoreVersion = "2.25.1"
+    const val mockitoInlineVersion = "2.13.0"
+    const val mockitoAndroidVersion = "2.23.4"
+    const val mockitoKotlinVersion = "2.1.0"
+    const val assertJVersion = "3.11.1"
+    const val extJunitVersion = "1.1.1"
+    const val androidTestRuleVersion = "1.1.0"
+    const val androidTestRunnerVersion = "1.1.0"
+    const val axTestCore = "1.1.0"
+    const val axTestJunit = "1.1.0"
+    const val espressoVersion = "3.2.0"
+    const val robolectricVersion = "4.5.1"
+
+}
