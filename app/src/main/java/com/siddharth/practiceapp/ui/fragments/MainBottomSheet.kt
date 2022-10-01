8404361318a68cde512faeb12f053459de17a2da
Rohit Jakhar <rohit@appflixstudio.com>
2022-10-01 13:31:08 +0530

    Repace MainFragments Adapter with List Adapter and optimised code.

--- app/src/main/java/com/siddharth/practiceapp/ui/fragments/MainBottomSheet.kt
+++ app/src/main/java/com/siddharth/practiceapp/ui/fragments/MainBottomSheet.kt
@@ -21,7 +21,7 @@ class MainBottomSheet : BottomSheetDialogFragment() {
     private var bottomSheetBehavior: BottomSheetBehavior<*>? = null
     private var _binding: FragmentBottomsheetHomeBinding? = null
     private val binding get() = _binding!!
-    private lateinit var mainAdapter: MainFragmentsAdapter
+    private val mainAdapter: MainFragmentsAdapter by lazy { MainFragmentsAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,7 +30,6 @@ class MainBottomSheet : BottomSheetDialogFragment() {
     ): View{
         _binding = FragmentBottomsheetHomeBinding.inflate(inflater,container,false)
         val root: View = binding.root
-        mainAdapter = MainFragmentsAdapter()
         setupUi()
         addDataToAdapter()
         return root
@@ -52,14 +51,17 @@ class MainBottomSheet : BottomSheetDialogFragment() {
         }
 
     private fun addDataToAdapter() {
-        mainAdapter.dataList.add(MainDataFrag("", "Basic Features", "", 2))
-        mainAdapter.dataList.add(MainDataFrag("Home", "", "basic", 1))
-        mainAdapter.dataList.add(MainDataFrag("TODO List", "", "basic",1))
-        mainAdapter.dataList.add(MainDataFrag("", "Pro Features", "", 2))
-        mainAdapter.dataList.add(MainDataFrag("PDF Reader", "", "basic", 1))
-        mainAdapter.dataList.add(MainDataFrag("Workout Reminder", "", "basic",1))
-        mainAdapter.dataList.add(MainDataFrag("Calories Calculator", "", "basic", 1))
-        mainAdapter.dataList.add(MainDataFrag("Yoga Teacher", "", "basic",1))
+        val itemList = listOf(
+            MainDataFrag("", "Basic Features", "", 2),
+            MainDataFrag("Home", "", "basic", 1),
+            MainDataFrag("TODO List", "", "basic",1),
+            MainDataFrag("", "Pro Features", "", 2),
+            MainDataFrag("PDF Reader", "", "basic", 1),
+            MainDataFrag("Workout Reminder", "", "basic",1),
+            MainDataFrag("Calories Calculator", "", "basic", 1),
+            MainDataFrag("Yoga Teacher", "", "basic",1)
+        )
+        mainAdapter.submitList(itemList)
         mainAdapter.notifyItemRangeChanged(0,8)
     }
 
